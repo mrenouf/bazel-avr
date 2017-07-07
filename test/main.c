3b717e56243bd3ebf9749ad00e3202677c24d690
Mark Renouf <mark.renouf@gmail.com>
2017-07-07 17:05:29 -0400

    Add some stdlib usage for the sanity check
    includes stdlib and calls malloc/free as well

--- test/main.c
+++ test/main.c
@@ -2,6 +2,7 @@
 #include <avr/eeprom.h>
 #include <avr/pgmspace.h>
 #include <util/delay.h>
+#include <stdlib.h>
 
 const char hello_message_flash[] PROGMEM = "This is a string held completely in flash (data).";
 const char hello_message_eeprom[] EEMEM = "This is a string held completely in ROM (eeprom).";
@@ -9,6 +10,8 @@ const char hello_message_ram[] = "This is a string loaded into RAM (code).";
 
 int main (void) {
 
+   void *data = malloc(100);
+   free(data);
    DDRB |= (1 << PB5);
 
    while(1) {
